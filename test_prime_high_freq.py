4c1499248299a13c03fd18fe0cd9eff2c688b1c4
antragoudaras <antonistragoudaras@gmail.com>
2022-06-17 18:01:27 +0300

    test prime/firefly results batchsize=1000 highfreq

--- test_prime_high_freq.py
+++ test_prime_high_freq.py
@@ -444,4 +444,4 @@ elif "train_dataset_" in args.load_dataset:
 elif "val_dataset_" in args.load_dataset:
 	prefix = "val_set"
 
-df.to_excel(f"{prefix}_freq_actual_results_rerun.xlsx")
+df.to_excel(f"{prefix}_freq_actual_results_1000_batch_size.xlsx")
