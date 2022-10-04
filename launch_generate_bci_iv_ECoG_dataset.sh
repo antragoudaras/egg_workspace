c09fd867a73ea69a4598b6f142f3ea307934d1de
antragoudaras <antonistragoudaras@gmail.com>
2022-10-04 19:16:02 +0300

    .xlsx file handling corrections

--- launch_generate_bci_iv_ECoG_dataset.sh
+++ launch_generate_bci_iv_ECoG_dataset.sh
@@ -15,7 +15,7 @@ for counter in {1..80..1}
 do
     JOB_NAME=bci_iv_ECoG_${counter}_out_of_80
     echo "$JOB_NAME"
-    sbatch --job-name "$JOB_NAME" "$SRC_DIR"/generate_bci_iv_ECoG_dataset.sbatch --save-dataset "${PROJECT_DIR}_${JOB_NAME}.xlsx"
+    sbatch --job-name "$JOB_NAME" "$SRC_DIR"/generate_bci_iv_ECoG_dataset.sbatch --save-dataset "${JOB_NAME}.xlsx"
 done
 
 echo "Generating 8000 lines of high freq dataset"
