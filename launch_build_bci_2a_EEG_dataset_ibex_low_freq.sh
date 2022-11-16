79f77f34ddee4f383060ae20a60ca2e7a9825646
antragoudaras <antonistragoudaras@gmail.com>
2022-11-16 19:33:44 +0300

    generate full dataset for BCI 2a EEG low freq

--- launch_build_bci_2a_EEG_dataset_ibex_low_freq.sh
+++ launch_build_bci_2a_EEG_dataset_ibex_low_freq.sh
@@ -8,11 +8,14 @@ PROJECT_DIR="$PWD"
 
 # project should have a src directory
 SRC_DIR="$PROJECT_DIR"
-JOB_RESULTS_DIR="$PROJECT_DIR"/building_baseline_bci_iv_2a_low_freq_log_outputs
+JOB_RESULTS_DIR="$PROJECT_DIR"/results_generate_bci_iv_2a_low_freq_dataset_contextual
 mkdir -p "$JOB_RESULTS_DIR"
 
-JOB_NAME=building_testing_bci_iv_2a_low_freq
-sbatch --job-name "$JOB_NAME" "$SRC_DIR"/build_bci_2a_EEG_dataset_ibex_low_freq.sbatch
+for counter in {1..80..1}
+do
+    JOB_NAME=bci_iv_2a_low_freq_${counter}_out_of_80_contextual_november
+    echo "$JOB_NAME"
+    sbatch --job-name "$JOB_NAME" "$SRC_DIR"/build_bci_2a_EEG_dataset_ibex_low_freq.sbatch --save-dataset "${JOB_NAME}.xlsx"
+done
 
-
-echo "Testing the baseline ShallowConvnet BCI 2a low freq..."
+echo "Generating 8000 architectures per subject/context of BCI 2a EEG low frequency dataset"
